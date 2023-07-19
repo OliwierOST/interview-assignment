d80ab547a66c144a2ec9f62d33319b7ef57d161e
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-07-19 22:20:42 +0200

    Update the styles of a button used for opening a modal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,23 @@ function App() {
 
   return (
     <Stack height="100vh" alignItems="center" justifyContent="center">
-      <Button variant="contained" onClick={() => setShowModal(true)}>
+      <Button
+        variant="contained"
+        onClick={() => setShowModal(true)}
+        sx={{
+          borderRadius: "6px",
+          borderColor: "#5870E0",
+          fontFamily: "Poppins",
+          fontSize: "16px",
+          fontWeight: 500,
+          textTransform: "none",
+          bgcolor: "#5870E0",
+          //add hover bg color slightly darker
+          ":hover": {
+            bgcolor: "#5978eb",
+          },
+        }}
+      >
         Open modal
       </Button>
       <PhoneSelectionModal showModal={showModal} setShowModal={setShowModal} />
