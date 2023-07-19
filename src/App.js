6c0291aa25719ac5c73c8b4f992aaa739c3ad945
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-07-19 20:15:10 +0200

    Add button to open a modal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Stack, Button } from "@mui/material"
+import { useState } from "react"
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Stack height="100vh" alignItems="center" justifyContent="center">
+      <Button variant="contained" onClick={() => setShowModal(true)}>
+        Open modal
+      </Button>
+    </Stack>
+  )
 }
 
-export default App;
+export default App
