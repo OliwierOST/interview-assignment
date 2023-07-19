40df6051bdbda0125b885d25514b0a8e2348b0ea
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2023-07-19 22:21:50 +0200

    Add test cases for leafs types other than string or () => string, or empty objects

--- src/tests/typedFreeze.test.ts
+++ src/tests/typedFreeze.test.ts
@@ -45,6 +45,30 @@ describe("typedFreeze", () => {
     // @ts-expect-error
     expect(() => (frozenArray[0] = "foo")).toThrow(TypeError)
     // @ts-expect-error
-    expect(() => (frozenArray[1] = "bar").toThrow(TypeError))
+    expect(() => (frozenArray[1] = "bar")).toThrow(TypeError)
+  })
+
+  it("doesn't work if any of the properties is other than string or () => string", () => {
+    expect(() => {
+      const invalidObject = { x: "foo", y: { a: 5, b: { c: () => "foo" } } }
+      const frozenInvalidObject = typedFreeze(invalidObject)
+      return frozenInvalidObject
+    }).toThrow(TypeError)
+    expect(() => {
+      const invalidObject = {
+        x: "foo",
+        y: { a: () => 5, b: { c: () => "foo" } },
+      }
+      const frozenInvalidObject = typedFreeze(invalidObject)
+      return frozenInvalidObject
+    }).toThrow(TypeError)
+  })
+
+  it("doesn't work if any of the properties is an empty object", () => {
+    expect(() => {
+      const invalidObject = { x: "foo", y: { a: {}, b: { c: () => "bar" } } }
+      const frozenInvalidObject = typedFreeze(invalidObject)
+      return frozenInvalidObject
+    }).toThrow(TypeError)
   })
 })
